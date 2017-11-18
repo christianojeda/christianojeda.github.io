cf2bdb370f1de83d049bcb83b61c0964962df9db
Nicolas <c.ojedadiaz@gmail.com>
2017-11-17 21:29:58 -0300

    contacto fondo

--- js/custom.js
+++ js/custom.js
@@ -84,3 +84,100 @@ $(function(){
 
 });
 
+
+
+
+
+
+
+
+//scroll
+
+$(document).ready(function(){
+
+  $(document).on("scroll", onScroll);
+
+    $('.vertical').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .stop().animate({scrollTop: targetOffset+1}, 1300, 'easeOutQuint');
+       return false;
+      }
+    }
+  });
+
+    $('#navbar li a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+        $('#navbar li').each(function(){
+            $(this).removeClass('active');
+        });
+      $(this).parent().addClass('active'); 
+
+        var target = this.hash,
+            menu = target;
+       $target = $(target);
+    });
+
+      $('a.panel').click(function() {
+
+    $('a.panel').removeClass('selected');
+    $(this).addClass('selected');
+
+    current = $(this);
+
+    $('#wrapper').scrollTo($(this).attr('href'), 800);
+
+    return false;
+  });
+
+  $(window).resize(function() {
+    resizePanel();
+  });
+
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navbar li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbar ul li').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            currLink.parent().removeClass("active");
+      console.log(currLink.parent());
+        }
+    });
+}
+
+function resizePanel() {
+
+  width = $(window).width();
+  height = $(window).height();
+
+  mask_width = width * $('.item').length;
+
+  $('#debug').html(width + ' ' + height + ' ' + mask_width);
+
+  $('#wrapper, .item').css({
+    width: width,
+    height: height
+  });
+  $('#mask').css({
+    width: mask_width,
+    height: height
+  });
+  $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+
+}
+
+
+
